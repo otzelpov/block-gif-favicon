e2b2fdd87648bb000229cfdb37aa2f5cfa39366f
otzelpov <hi@otzelpov.xyz>
2023-04-28 20:40:44 +0200

    init commit

---
+++ block.js
@@ -0,0 +1,23 @@
+// :-)
+
+function findFavIcon(){
+    var link = document.querySelector("link[rel~='icon']");
+    
+    if (!link) {
+        link = document.createElement('link');
+        link.rel = 'icon';
+        document.head.appendChild(link);
+    }
+
+    return link    
+}
+
+function isAnimated(link){
+    return link.href.includes('.gif')
+}
+
+link = findFavIcon()
+
+if(isAnimated(link)){
+    link.href = 'https://stackoverflow.com/favicon.ico';
+}
